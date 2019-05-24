7222c7021c1ecd24382ff0ca77ef3b0b4f5f1f50
mu ali <elshrab90.ma@gamil.com>
2019-05-25 01:10:06 +0200

    starting hooks todo app

---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+function Todo(){
+    
+
+
+
+    return(
+        <div>This will be the todo app</div>
+    )
+}
+
+export default Todo;
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './Todo'
+
+
+function TodoList(){
+
+
+    return (
+        <Todo />
+    )
+}
+
+export default TodoList;
